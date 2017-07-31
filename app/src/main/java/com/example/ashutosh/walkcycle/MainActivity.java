a9c138191d63f634fedceb5ce2eea1eccd0d3197
Ashutosh <ashutoshmca13@gmail.com>
2017-08-01 00:14:50 +0530

    Simple KeyFrameAnimtion in android

---
+++ app/src/main/java/com/example/ashutosh/walkcycle/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ashutosh.walkcycle;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
+    private AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.animationshow);
+        animationDrawable = (AnimationDrawable) img.getDrawable();
+        img.post(new Runnable() {
+            @Override
+            public void run() {
+                animationDrawable.start();
+            }
+        });
+
+
+    }
+}
